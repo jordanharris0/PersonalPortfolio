338eb5159a0a9bb09c39c8ce5296f64a8968b11a
jordanharris0 <jordanharris1833@yahoo.com>
2024-10-10 14:26:58 -0500

    finally finished the particle system and hero component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
+import Canvas from "./components/Canvas";
 
 function App() {
   return (
     <>
       <Hero />
+      {/* <Canvas /> */}
     </>
   );
 }
