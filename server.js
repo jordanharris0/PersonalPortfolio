4b9971e10991d06de2bfa4a86700e6cbe813b755
jordanharris0 <jordanharris1833@yahoo.com>
2024-10-15 17:42:34 -0500

    added cors to server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const contactRoutes = require("./prisma");
+const cors = require("cors");
 
 const app = express();
 
 //middleware
 app.use(bodyParser.json());
 app.use(require("morgan")("dev"));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Only allow your frontend origin
+    credentials: true, // Allow credentials such as Authorization headers or cookies
+  })
+);
 
 //contact form route
 app.use("/contact", contactRoutes);
